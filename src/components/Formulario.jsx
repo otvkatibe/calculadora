906790536ebbbd2a21fbcb0830ccc0d746747df4
Otávio Katibe <otaviokatibe@gmail.com>
2025-03-27 14:59:06 -0300

    Update Formulario.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,31 +7,56 @@ const Formulario = ({ onCalcular }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        onCalcular ({
+        onCalcular({
             dia: parseInt(dia, 10),
             mes: parseInt(mes, 10),
             ano: parseInt(ano, 10),
         });
     };
 
+    const handleChange = (e, setter) => {
+        const value = e.target.value;
+        
+        if (/^\d*$/.test(value)) {
+            setter(value);
+        }
+    };
+
     return (
         <form onSubmit={handleSubmit}>
             <div>
                 <label>Dia</label>
-                <input type = 'number' placeholder = "" value = {dia} onChange = {(e) => setDia(e.target.value)} required/>
+                <input 
+                    type='text'
+                    value={dia} 
+                    onChange={(e) => handleChange(e, setDia)} 
+                    required 
+                    placeholder="Digite o dia" 
+                />
             </div>
             <div>
                 <label>Mês</label>
-                <input type = 'number' placeholder = "" value = {mes} onChange = {(e) => setMes(e.target.value)} required/>
+                <input 
+                    type='text' 
+                    value={mes} 
+                    onChange={(e) => handleChange(e, setMes)} 
+                    required 
+                    placeholder="Digite o mês" 
+                />
             </div>
             <div>
                 <label>Ano</label>
-                <input type = 'number' placeholder = "" value = {ano} onChange = {(e) => setAno(e.target.value)} required/>
+                <input 
+                    type='text' 
+                    value={ano} 
+                    onChange={(e) => handleChange(e, setAno)} 
+                    required 
+                    placeholder="Digite o ano" 
+                />
             </div>
-            <button type = "submit" >↓</button>
+            <button type="submit">↓</button>
         </form>
     );
-
 };
 
 export default Formulario;
